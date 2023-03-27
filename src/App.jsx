ada094d20b775730db68bf5eb653f10d8645b0b6
Vinnakota Priyatam - v0p005f <vinnakota.priyatam@walmart.com>
2023-03-27 12:03:14 +0530

    made appId and blogId dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CommentsSection from "./CommentsSection";
 
 function App() {
-  const applicationId = 123;
-  const blogId = 12;
+  const [applicationId, setApplicationId] = useState(null);
+  const [blogId, setBlogId] = useState(null);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    setApplicationId(searchParams.get('applicationId'));
+    setBlogId(searchParams.get('blogId'));
+
+  }, []);
+
+  console.log('here', applicationId, blogId);
 
   return (
     <div className="app">
-      <CommentsSection
-        applicationId={applicationId}
-        blogId={blogId}
-      />
+      {
+        applicationId && blogId &&
+          <CommentsSection
+            applicationId={applicationId}
+            blogId={blogId}
+          />
+      }
     </div>
   );
 }
--- src/CommentsSection.js
+++ src/CommentsSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
 
